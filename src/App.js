9e17269fde93af9067de44cf2a7894c9c6b40555
mewmaze <mewmaze@naver.com>
2024-08-04 23:36:09 +0900

    feat: 라우팅 설정추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/top" element={<Top/>}/>
+          <Route path="/findmeme" element={<FindMeme/>}/>
+          <Route path="/mypage/:id" element={<MyPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
