6662fab3a6becf6502712d9ebbb7695137db90be
mewmaze <mewmaze@naver.com>
2024-08-06 11:56:40 +0900

    feat: app.js에 AuthProvider로 감싸기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Main/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/top" element={<Top/>}/>
-          <Route path="/findmeme" element={<FindMeme/>}/>
-          <Route path="/mypage/:id" element={<MyPage/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+        {/* <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/findmeme" element={<FindMeme/>}/>
+        <Route path="/mypage/:id" element={<MyPage/>}/> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
+
   );
 }
 
