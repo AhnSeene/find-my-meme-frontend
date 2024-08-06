c392d1408741d8d03d119660bb43b0596a18bb8d
mewmaze <mewmaze@naver.com>
2024-08-06 13:19:36 +0900

    feat: 헤더 생성

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import './Header.css';
+
+function Header (){
+    const navigate = useNavigate();
+    const { isLoggedIn } = useAuth(); 
+    console.log('isLoggedIn :',isLoggedIn);
+    
+    const handleLogin = () => {
+        navigate('/login',{ replace:true });
+    };
+
+    return(
+        <div className="Header">
+            <Link to="/" className="logo-link">
+                <h1>로고</h1>
+            </Link>
+            <div className="nav-buttons">
+                {isLoggedIn? (
+                    <>
+                        <button>짤등록</button>
+                        <button>마이페이지</button>
+                    </>
+                ):(
+                    <>
+                        <button>회원가입</button>
+                        <button onClick={handleLogin}>로그인</button>
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+export default Header;
