e67ff3c4bdc9ee7d1bdb8ae2cea1128cbb310a3e
mewmaze <mewmaze@naver.com>
2024-08-16 00:55:55 +0900

    feat: findmemepost에 작성시간 보이게 수정, 페이징 기능 구현

--- src/pages/FindMeme.js
+++ src/pages/FindMeme.js
@@ -3,7 +3,21 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import './findmeme.css';
+import { CgSearch } from "react-icons/cg";
+import { CgSearchFound } from "react-icons/cg";
 
+// 날짜 포맷팅 함수
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}.${month}.${day} ${hours}:${minutes}`;
+};
 
 function FindMeme(){
     const navigate = useNavigate();
@@ -11,71 +25,36 @@ function FindMeme(){
     const [foundPost,setFoundPost] = useState([]);
     const [isFindActive,setIsFindActive] = useState(true);
     const [loading,setLoading]=useState(false);
+    const [currentPage, setCurrentPage] = useState(0); // 현재 페이지 상태
+    const [totalPages, setTotalPages] = useState(0); // 총 페이지 상태
 
-    // 더미 데이터
-    const mockFindPost = [
-        {
-            id:1,
-            title: "더미 찾아줘 밈 1",
-            content: "이것은 첫 번째 밈의 내용입니다. 길이가 길 수 있습니다. 더 길어질 수 있습니다. 그래서 일부만 보여줄 것입니다.",
-            imageUrl: "https://via.placeholder.com/150"
-        },
-        {
-            id: 2,
-            title: "더미 찾아줘 밈 2",
-            content: "두 번째 밈의 간단한 내용입니다.",
-            imageUrl: null
-        },
-        {
-            id: 3,
-            title: "더미 찾아줘 밈 3",
-            content: "세 번째 밈의 간단한 내용입니다.",
-            imageUrl: "https://via.placeholder.com/150"
-        },
-    ];
-
-    const mockFoundPost = [
-        {
-            id: 4,
-            title: "더미 찾았다 밈 1",
-            content: "첫 번째 찾았다 밈의 간단한 내용입니다.",
-            imageUrl: null
-        },
-        {
-            id: 5,
-            title: "더미 찾았다 밈 2",
-            content: "두 번째 찾았다 밈의 간단한 내용입니다.",
-            imageUrl: "https://via.placeholder.com/150"
-        },
-    ];
-
-    // // 공통 데이터 로딩 함수
-    // const fetchData = async (url, setData) => {
-    //     setLoading(true);
-    //     try {
-    //         const response = await axios.get(url);
-    //         setData(response.data);
-    //     } catch (error) {
-    //         console.error("Error fetching data:", error);
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // };
+    // 공통 데이터 로딩 함수
+    const fetchData = async (url, setData) => {
+        setLoading(true);
+        try {
+            const response = await axios.get(url);
+            setData(response.data.data.content);
+            setTotalPages(response.data.data.totalPages);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        // fetchData('/api/find', setFindPost);
-        setFindPost(mockFindPost);
-    }, []);
+        fetchData(`http://localhost:8080/api/v1/find-posts?page=${currentPage}&size=3`, setFindPost);
+    }, [currentPage]);
 
     const handleFind = () => {
-        // fetchData('/api/find', setFindPost);
-        setFindPost(mockFindPost);
+        fetchData('/api/find', setFindPost);
+        // setFindPost(mockFindPost);
         setIsFindActive(true);
     }
 
     const handleFound = () => {
-        // fetchData('/api/found', setFoundPost);
-        setFoundPost(mockFoundPost);
+        fetchData('/api/found', setFoundPost);
+        // setFoundPost(mockFoundPost);
         setIsFindActive(false);
     }
 
@@ -83,56 +62,73 @@ function FindMeme(){
         navigate('/findmemepost',{replace:true})
     }
 
-    const truncateContent = (content,maxLength) => {
-        if(content.length <= maxLength) {
-            return content;
+    const handlePageChange = (page) => {
+        if (page >= 0 && page < totalPages) {
+            setCurrentPage(page);
         }
-        return content.slice(0, maxLength) + '...';
-    }
+    };
 
     return(
         <div className="findmeme">
-            <div>
-                <button onClick={handleFind}>찾아줘</button>
-                <button onClick={handleFound}>찾았다</button>
+            <div className="findmeme-btn">
+                <button onClick={handleFind}><CgSearch size={32}  /> 찾아줘</button>
+                <button onClick={handleFound}> <CgSearchFound size={32} /> 찾았다</button>
             </div>
-            <button onClick={handlePost}>글 등록</button>
-            <div>
-                {loading && <p>Loading...</p>}
-                {isFindActive ? (
-                    <div className="findmeme-posts">
-                        {findPost.length > 0 ? (
-                            findPost.map((post, index) => (
-                                <div key={index} className="post-summary">
-                                    <Link to={`/post/${post.id}`}>
-                                        <h3>{post.title}</h3>
-                                        <p>{truncateContent(post.content, 20)}</p>
-                                        {post.imageUrl && <img src={post.imageUrl} alt={post.title} width="100" />}
-                                    </Link>
-                                </div>
-                            ))
-                        ) : (
-                            <p>찾아줘 게시물 없음</p>
-                        )}
-                    </div>
-                ) : (
-                    <div className="findmeme-posts">
-                        {foundPost.length > 0 ? (
-                            foundPost.map((post, index) => (
-                                <div key={index} className="post-summary">
-                                    <Link to={`/post/${post.id}`}>
-                                        <h3>{post.title}</h3>
-                                        {post.imageUrl && <img src={post.imageUrl} alt={post.title} width="100" />}
-                                        <p>{truncateContent(post.content, 100)}</p>
-                                    </Link>
-                                </div>
-                            ))
-                        ) : (
-                            <p>찾았다 게시물 없음</p>
-                        )}
-                    </div>
-                )}
-            </div>
+            <button className="findmeme-post-btn" onClick={handlePost}>글 등록</button>
+            {loading && <p>Loading...</p>}
+            {isFindActive ? (
+                <div className="findmeme-posts">
+                    {findPost.length > 0 ? (
+                        findPost.map((post, index) => (
+                            <div key={index} className="post-summary">
+                                <Link to={`/findmeme/${post.id}`}>
+                                    <div className="findmeme-posts-title">{post.title}</div>
+                                    <div className="findmeme-posts-content">{post.content}</div>
+                                    <div className="findmeme-posts-other">
+                                        <div>{post.username}</div>
+                                        <div>{formatDate(post.createdAt)}</div>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <p>찾아줘 게시물 없음</p>
+                    )}
+                </div>
+            ) : (
+                <div className="findmeme-posts">
+                    {foundPost.length > 0 ? (
+                        foundPost.map((post, index) => (
+                            <div key={index} className="post-summary">
+                                <Link to={`/findmeme/${post.id}`}>
+                                    <div className="findmeme-posts-title">{post.title}</div>
+                                    <div className="findmeme-posts-content">{post.content}</div>
+                                    <div className="findmeme-posts-other">
+                                        <div>{post.username}</div>
+                                        <div>{formatDate(post.createdAt)}</div>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <p>찾았다 게시물 없음</p>
+                    )}
+                </div>
+            )}
+            {/* 페이지네이션 버튼 추가 */}
+            {totalPages > 1 && !loading && (
+                <div className="pagination">
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => handlePageChange(index)}
+                            className={index === currentPage ? 'active' : ''}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
