4702f594180fb685b69037d70e7a908af9505173
mewmaze <mewmaze@naver.com>
2024-08-29 03:43:03 +0900

    feat: 로그인, 로그아웃 기능 구현

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,18 +1,43 @@
-import { createContext,useState,useContext} from "react";
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({children})=>{
-    const [isLoggedIn,setIsLoggedIn] =useState(false);
+export function AuthProvider({ children }) {
+  const [authState, setAuthState] = useState({
+    isLoggedIn: false,
+    token: null,
+    username: null,  // username 필드 추가
+  });
 
-    const login = () => setIsLoggedIn(true);
-    const logout = () => setIsLoggedIn(false);
+  // 페이지 로드 시 로컬 스토리지에서 토큰과 username을 가져와 상태 복원
+  useEffect(() => {
+    const storedToken = localStorage.getItem('jwtToken');
+    const storedUsername = localStorage.getItem('username');  // username 가져오기
+    if (storedToken && storedUsername) {
+      setAuthState({ isLoggedIn: true, token: storedToken, username: storedUsername });
+    }
+  }, []);
 
-    return(
-        <AuthContext.Provider value={{isLoggedIn,login,logout}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  const login = (token, username) => {
+    setAuthState({ isLoggedIn: true, token, username });
+    localStorage.setItem('jwtToken', token);
+    localStorage.setItem('username', username);  // username 저장
+  };
+
+  const logout = () => {
+    setAuthState({ isLoggedIn: false, token: null, username: null });
+    localStorage.removeItem('jwtToken'); // JWT 토큰 제거
+    localStorage.removeItem('username'); // username 제거
+  };
+
+  return (
+    <AuthContext.Provider value={{ authState, setAuthState, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export const useAuth = () => useContext(AuthContext);
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
