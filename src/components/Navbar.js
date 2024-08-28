007cb7b8d48deabbf4002af653106927cdb61e6c
mewmaze <mewmaze@naver.com>
2024-08-29 03:59:25 +0900

    design: Navbar 항목 이름 변경

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ function Navbar(){
     return(
         <div className="navbar">
             <ul>
-                <li><Link to='/'>모든 표현</Link></li>
-                <li><Link to='/'>이번주 Top</Link></li>
-                <li><Link to='/findmeme'>내 표현을 찾아줘</Link></li>
+                <li><Link to='/'>ALL MEME</Link></li>
+                <li><Link to='/topmeme'>Top MEME</Link></li>
+                <li><Link to='/findmeme'>Find MEME</Link></li>
             </ul>
         </div>
     )   
