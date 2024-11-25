cdcc5153dea0c325cecad309bea287be6946f7ef
mewmaze <mewmaze@naver.com>
2024-11-25 21:49:01 +0900

    role 추가

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -7,30 +6,32 @@ export function AuthProvider({ children }) {
   const [authState, setAuthState] = useState({
     isLoggedIn: false,
     token: null,
-    username: null,  // username 필드 추가
-    roles: [] 
+    username: null, // username 필드 추가
   });
-  const navigate = useNavigate();
-  
+
   // 페이지 로드 시 로컬 스토리지에서 토큰과 username을 가져와 상태 복원
   useEffect(() => {
-    const storedToken = localStorage.getItem('jwtToken');
-    const storedUsername = localStorage.getItem('username');  // username 가져오기
+    const storedToken = localStorage.getItem("jwtToken");
+    const storedUsername = localStorage.getItem("username"); // username 가져오기
     if (storedToken && storedUsername) {
-      setAuthState({ isLoggedIn: true, token: storedToken, username: storedUsername });
+      setAuthState({
+        isLoggedIn: true,
+        token: storedToken,
+        username: storedUsername,
+      });
     }
   }, []);
 
   const login = (token, username) => {
     setAuthState({ isLoggedIn: true, token, username });
-    localStorage.setItem('jwtToken', token);
-    localStorage.setItem('username', username);  // username 저장
+    localStorage.setItem("jwtToken", token);
+    localStorage.setItem("username", username); // username 저장
   };
 
   const logout = () => {
     setAuthState({ isLoggedIn: false, token: null, username: null });
-    localStorage.removeItem('jwtToken'); // JWT 토큰 제거
-    localStorage.removeItem('username'); // username 제거
+    localStorage.removeItem("jwtToken"); // JWT 토큰 제거
+    localStorage.removeItem("username"); // username 제거
   };
 
   return (
@@ -43,4 +44,3 @@ export function AuthProvider({ children }) {
 export function useAuth() {
   return useContext(AuthContext);
 }
-
