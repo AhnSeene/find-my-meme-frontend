b09e38e45708616c8c742b65f35a0b78a6ec83ac
mewmaze <mewmaze@naver.com>
2024-11-25 21:56:59 +0900

    Button  공통 컴포넌트로 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,39 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import './Navbar.css';
-function Navbar(){
-    const [search,setSearch] = useState('');
+import Button from "./Button";
+import "./Navbar.css";
+function Navbar() {
+  const [search, setSearch] = useState("");
 
-    const handleSearch = () => {
-        if(search.trim()) {
-            console.log('검색어:', search);
-        } else {
-            console.log('검색어를 입력하세요');
-        }
+  const handleSearch = () => {
+    if (search.trim()) {
+      console.log("검색어:", search);
+    } else {
+      console.log("검색어를 입력하세요");
     }
-    return(
-        <div className="navbar">
-            <ul>
-                <li><Link to='/'>모든 표현</Link></li>
-                <li><Link to='/topmeme'>인기 표현</Link></li>
-                <li><Link to='/findmeme'>내 표현을 찾아줘</Link></li>
-            </ul>
-            <input 
-                className="navbar-input"
-                type="text" 
-                placeholder="검색어 입력"
-                value={search}
-                onChange={(e)=>setSearch(e.target.value)}
-            />
-            <button onClick={handleSearch}>검색</button>
-        </div>
-    )   
+  };
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link to="/">모든 표현</Link>
+        </li>
+        <li>
+          <Link to="/topmeme">인기 표현</Link>
+        </li>
+        <li>
+          <Link to="/findmeme">내 표현을 찾아줘</Link>
+        </li>
+      </ul>
+      <input
+        className="navbar-input"
+        type="text"
+        placeholder="검색어 입력"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Button onClick={handleSearch} text="검색" />
+    </div>
+  );
 }
 export default Navbar;
