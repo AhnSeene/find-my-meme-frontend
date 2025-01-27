6de7b49aaa279b62903665ffeb21ba2c3c0c83fe
mewmaze <mewmaze@naver.com>
2025-01-27 19:39:49 +0900

    메인페이지에서 좋아요 누를때 reactquery 사용하는것으로 수정

--- src/hooks/useToggleLike.js
+++ src/hooks/useToggleLike.js
@@ -1,31 +1,57 @@
-// src/hooks/useToggleLike.js
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../contexts/api";
+const useToggleLike = ({
+  selectedSubTags = [],
+  isProfile = false,
+  username = "",
+  authState,
+}) => {
+  const queryClient = useQueryClient();
 
-const useToggleLike = (memes, setMemes, authState) => {
-  const toggleLike = async (event, memeId) => {
-    event.stopPropagation();
-    const updatedMemes = memes.map((meme) => {
-      if (meme.id === memeId) {
-        const isLiked = !meme.isLiked;
-        const likeCount = isLiked ? meme.likeCount + 1 : meme.likeCount - 1;
-        return { ...meme, isLiked, likeCount };
-      }
-      return meme;
-    });
-    setMemes(updatedMemes);
-
-    try {
-      await api.post(
+  return useMutation({
+    mutationFn: ({ memeId, isLiked }) => {
+      return api.post(
         `/meme-posts/${memeId}/toggleLike`,
         {},
         { headers: { Authorization: `Bearer ${authState.token}` } }
       );
-    } catch (error) {
-      console.error("Failed to toggle like:", error);
-    }
-  };
+    },
+    onMutate: async ({ memeId, isLiked }) => {
+      const queryKey = ["memes", { selectedSubTags, isProfile, username }];
 
-  return toggleLike;
+      // 이전 데이터 가져오기
+      const previousData = queryClient.getQueryData(queryKey);
+
+      queryClient.setQueryData(queryKey, (oldData) => {
+        if (!oldData) return oldData;
+
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => ({
+            ...page,
+            content: page.content.map((meme) => {
+              if (meme.id === memeId) {
+                return {
+                  ...meme,
+                  isLiked: !isLiked, // 반전된 isLiked 상태
+                  likeCount: isLiked ? meme.likeCount - 1 : meme.likeCount + 1, // likeCount 변경
+                };
+              }
+              return meme;
+            }),
+          })),
+        };
+      });
+
+      // 이전 데이터 저장 (onError에서 롤백할 때 사용)
+      return { previousData, queryKey };
+    },
+    onError: (error, { memeId, isLiked }, context) => {
+      // 요청 실패 시 롤백
+      queryClient.setQueryData(context.queryKey, context.previousData);
+    },
+    onSettled: (_, __, { memeId, isLiked }, context) => {},
+  });
 };
 
 export default useToggleLike;
