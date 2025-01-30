6eabff321989172fd5ab33e5f73f94314d4c2c83
mewmaze <mewmaze@naver.com>
2025-01-30 22:06:23 +0900

    로고 클릭시 새로고침 & pointer설정

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { useAuth } from "../contexts/AuthContext";
 import logo from "../assets/logo.png";
 import "./Header.css";
 
-// 맨 위 Header 
+// 맨 위 Header
 // 로그인 전에 회원가입/로그인 버튼, 로그인 후 짤등록/마이페이지 버튼
 function Header() {
   const navigate = useNavigate();
@@ -32,12 +32,19 @@ function Header() {
     navigate("/", { replace: true });
   };
 
+  const handleLogoClick = () => {
+    window.location.href = "/"; //새로고침하면서 홈으로 이동
+  };
+
   return (
     <div className="Header">
       <h1 className="logo">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="Find My Meme 로고" className="logo-image" />
-        </Link>
+        <img
+          src={logo}
+          alt="Find My Meme 로고"
+          className="logo-image"
+          onClick={handleLogoClick}
+        />
       </h1>
       <div className="nav-buttons">
         {authState.isLoggedIn ? (
@@ -49,7 +56,9 @@ function Header() {
                 <button onClick={handleMyPage}>나의 밈</button>
                 <button onClick={handleMyPage}>내 정보</button>
                 <button onClick={handleMyPage}>게시글 관리</button>
-                <button id="logout" onClick={handleLogout}>로그아웃</button>
+                <button id="logout" onClick={handleLogout}>
+                  로그아웃
+                </button>
               </div>
             </div>
           </>
