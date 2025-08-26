ed961e135fef8aeab9495d2488fe89fb3e8a44ba
mewmaze <mewmaze@naver.com>
2025-08-27 04:37:54 +0900

    feat: 필터 영역 UI/UX 개선

--- src/components/filter/FilterBar.js
+++ src/components/filter/FilterBar.js
@@ -1,11 +1,16 @@
 import TagSelector from "../tag/TagSelector";
 import TypeFilter from "./TypeFilter";
+import "./FilterBar.css";
 
 export default function FilterBar() {
   return (
     <div className="filter-bar">
-      <TagSelector />
-      <TypeFilter />
+      <div className="tag-selector">
+        <TagSelector />
+      </div>
+      <div className="type-filter">
+        <TypeFilter />
+      </div>
     </div>
   );
 }
