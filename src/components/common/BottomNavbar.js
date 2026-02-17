5be0884b927fbe45364508a7441df04609702f51
mewmaze <mewmaze@naver.com>
2026-02-17 13:52:39 +0900

    refactor: 레이아웃 수정

--- src/components/common/BottomNavbar.js
+++ src/components/common/BottomNavbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { IoSearch } from "react-icons/io5";
 import { AiFillHome } from "react-icons/ai";
@@ -9,34 +9,24 @@ import useAuthStore from "../../store/useAuthStore";
 function BottomNavbar() {
   const usernameFromStore = useAuthStore((state) => state.username);
   return (
-    <div className="bottomnavbar">
-      <ul>
-        <li>
-          <Link to="/">
-            <AiFillHome className="icon" />
-            <span>모든표현</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/topmeme">
-            <FiThumbsUp className="icon" />
-            <span>인기표현</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/findmeme">
-            <IoSearch className="icon" />
-            <span>내 표현을 찾아줘</span>
-          </Link>
-        </li>
-        <li>
-          <Link to={`/users/${usernameFromStore}`}>
-            <CgProfile className="icon" />
-            <span>마이페이지</span>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <nav className="bottomnavbar">
+      <NavLink to="/" end className={({ isActive }) => `bottomnavbar-item ${isActive ? "active" : ""}`}>
+        <AiFillHome className="bottomnavbar-icon" />
+        <span>모든표현</span>
+      </NavLink>
+      <NavLink to="/topmeme" className={({ isActive }) => `bottomnavbar-item ${isActive ? "active" : ""}`}>
+        <FiThumbsUp className="bottomnavbar-icon" />
+        <span>인기표현</span>
+      </NavLink>
+      <NavLink to="/findmeme" className={({ isActive }) => `bottomnavbar-item ${isActive ? "active" : ""}`}>
+        <IoSearch className="bottomnavbar-icon" />
+        <span>표현찾기</span>
+      </NavLink>
+      <NavLink to={`/users/${usernameFromStore}`} className={({ isActive }) => `bottomnavbar-item ${isActive ? "active" : ""}`}>
+        <CgProfile className="bottomnavbar-icon" />
+        <span>마이페이지</span>
+      </NavLink>
+    </nav>
   );
 }
 export default BottomNavbar;
